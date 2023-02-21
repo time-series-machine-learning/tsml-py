b41e3c4e2943e836fa48d7bbc2a08117ac9ab457
Matthew Middlehurst <m.middlehurst@uea.ac.uk>
2023-02-21 20:56:16 +0000

    testing

--- tsml/tests/test_all_estimators.py
+++ tsml/tests/test_all_estimators.py
@@ -7,7 +7,7 @@ __author__ = ["MatthewMiddlehurst"]
 from tsml.utils.testing import generate_test_estimators, parametrize_with_checks
 
 
-@parametrize_with_checks(generate_test_estimators())
+@parametrize_with_checks(sorted(generate_test_estimators()))
 def test_check_estimator(estimator, check):
     """Test that sklearn estimators adhere to sklearn conventions."""
     check(estimator)
