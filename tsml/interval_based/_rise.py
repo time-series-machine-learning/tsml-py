555b0454958f01971c14c0cd4651763415d2c9e7
Matthew Middlehurst <m.middlehurst@uea.ac.uk>
2023-07-20 22:52:58 +0100

    param bugfix

--- tsml/interval_based/_rise.py
+++ tsml/interval_based/_rise.py
@@ -163,7 +163,7 @@ class RISEClassifier(ClassifierMixin, BaseIntervalForest):
             replace_nan = 0
 
         interval_features = [
-            PeriodogramTransformer(use_pyfftw=True, pad_with="mean"),
+            PeriodogramTransformer(use_pyfftw=use_pyfftw, pad_with="mean"),
             AutocorrelationFunctionTransformer(
                 n_lags=acf_lag, min_values=acf_min_values
             ),
