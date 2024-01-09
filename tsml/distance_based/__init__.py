518f0a1c81d5901f11872dc6f396f3db36bbf5c0
Matthew Middlehurst <pfm15hbu@gmail.com>
2024-01-09 12:47:02 +0000

    Grail classifier (#25)
    * fixes
    
    * commed mypy
    
    * c22
    
    * transform no longer requires y
    
    * fixes
    
    * fixes
    
    * fixes
    
    * grail
    
    * grail
    
    * comment

--- tsml/distance_based/__init__.py
+++ tsml/distance_based/__init__.py
@@ -1,9 +1,11 @@
 """Distance-based estimators."""
 
 __all__ = [
+    "GRAILClassifier",
     "ProximityForestClassifier",
     "MPDistClassifier",
 ]
 
+from tsml.distance_based._grail import GRAILClassifier
 from tsml.distance_based._mpdist import MPDistClassifier
 from tsml.distance_based._pf import ProximityForestClassifier
