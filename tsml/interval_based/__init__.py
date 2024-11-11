2ae58c75f49b4011e3b57f621503c671fb926973
Matthew Middlehurst <m.middlehurst@uea.ac.uk>
2024-11-11 22:15:06 +0000

    aeon estimator purge

--- tsml/interval_based/__init__.py
+++ tsml/interval_based/__init__.py
@@ -2,30 +2,14 @@
 
 __all__ = [
     "BaseIntervalForest",
-    "CIFClassifier",
-    "CIFRegressor",
-    "DrCIFClassifier",
-    "DrCIFRegressor",
     "IntervalForestClassifier",
     "IntervalForestRegressor",
     "RandomIntervalClassifier",
     "RandomIntervalRegressor",
     "SupervisedIntervalClassifier",
-    "RISEClassifier",
-    "RISERegressor",
-    "STSFClassifier",
-    "RSTSFClassifier",
-    "TSFClassifier",
-    "TSFRegressor",
 ]
 
 from tsml.interval_based._base import BaseIntervalForest
-from tsml.interval_based._cif import (
-    CIFClassifier,
-    CIFRegressor,
-    DrCIFClassifier,
-    DrCIFRegressor,
-)
 from tsml.interval_based._interval_forest import (
     IntervalForestClassifier,
     IntervalForestRegressor,
@@ -35,6 +19,3 @@ from tsml.interval_based._interval_pipelines import (
     RandomIntervalRegressor,
     SupervisedIntervalClassifier,
 )
-from tsml.interval_based._rise import RISEClassifier, RISERegressor
-from tsml.interval_based._stsf import RSTSFClassifier, STSFClassifier
-from tsml.interval_based._tsf import TSFClassifier, TSFRegressor
--- tsml/transformations/__init__.py
+++ tsml/transformations/__init__.py
@@ -3,7 +3,6 @@
 __all__ = [
     "AutocorrelationFunctionTransformer",
     "ARCoefficientTransformer",
-    "Catch22Transformer",
     "FPCATransformer",
     "FunctionTransformer",
     "RandomIntervalTransformer",
@@ -11,16 +10,12 @@ __all__ = [
     # "FixedIntervalTransformer",
     "PeriodogramTransformer",
     # "QuantileTransformer",
-    # "SFATransformer",
-    "RandomShapeletTransformer",
-    "RandomDilatedShapeletTransformer",
     "SevenNumberSummaryTransformer",
     "TransformerConcatenator",
 ]
 
 from tsml.transformations._acf import AutocorrelationFunctionTransformer
 from tsml.transformations._ar_coefficient import ARCoefficientTransformer
-from tsml.transformations._catch22 import Catch22Transformer
 from tsml.transformations._fpca import FPCATransformer
 from tsml.transformations._function_transformer import FunctionTransformer
 from tsml.transformations._interval_extraction import (
@@ -28,9 +23,5 @@ from tsml.transformations._interval_extraction import (
     SupervisedIntervalTransformer,
 )
 from tsml.transformations._periodogram import PeriodogramTransformer
-from tsml.transformations._shapelet_transform import (
-    RandomDilatedShapeletTransformer,
-    RandomShapeletTransformer,
-)
 from tsml.transformations._summary_features import SevenNumberSummaryTransformer
 from tsml.transformations._transform_concatenator import TransformerConcatenator
